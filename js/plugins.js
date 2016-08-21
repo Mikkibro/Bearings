b948a8e7820c2d2183cd3cd4ee040c8e4c59ef06
Mikkibro <mykolateterya@seznam.cz>
2016-08-21 11:57:29 +0300

    fix

--- js/plugins.js
+++ js/plugins.js
@@ -109,9 +109,9 @@
 
 
 		/**
-		 * ===================================================================================
+		 *
 		 * = PRIVATE FUNCTIONS
-		 * ===================================================================================
+		 *
 		 */
 
 		/**
@@ -1320,12 +1320,9 @@
 
 
 
-/**
- * Single Page Nav Plugin
- * Copyright (c) 2013 Chris Wojcik <hello@chriswojcik.net>
- * Dual licensed under MIT and GPL.
- * @author Chris Wojcik
- * @version 1.1.0
+/*
+  Single Page Nav Plugin
+
  */
 
 // Utility
@@ -1339,14 +1336,14 @@ if (typeof Object.create !== 'function') {
 
 (function($, window, document, undefined) {
     "use strict";
-    
+
     var SinglePageNav = {
-        
+
         init: function(options, container) {
-            
+
             this.options = $.extend({}, $.fn.singlePageNav.defaults, options);
-            
-            this.container = container;            
+
+            this.container = container;
             this.$container = $(container);
             this.$links = this.$container.find('a');
 
@@ -1356,7 +1353,7 @@ if (typeof Object.create !== 'function') {
 
             this.$window = $(window);
             this.$htmlbody = $('html, body');
-            
+
             this.$links.on('click.singlePageNav', $.proxy(this.handleClick, this));
 
             this.didScroll = false;
@@ -1369,7 +1366,7 @@ if (typeof Object.create !== 'function') {
                 link  = e.currentTarget,
                 $elem = $(link.hash);
 
-            e.preventDefault();             
+            e.preventDefault();
 
             if ($elem.length) { // Make sure the target elem exists
 
@@ -1382,8 +1379,8 @@ if (typeof Object.create !== 'function') {
                 }
 
                 self.setActiveLink(link.hash);
-                
-                self.scrollTo($elem, function() { 
+
+                self.scrollTo($elem, function() {
 
                     if (self.options.updateHash && history.pushState) {
                         history.pushState(null,null, link.hash);
@@ -1395,20 +1392,20 @@ if (typeof Object.create !== 'function') {
                     if (typeof self.options.onComplete === 'function') {
                         self.options.onComplete();
                     }
-                });                            
-            }     
+                });
+            }
         },
-        
+
         scrollTo: function($elem, callback) {
             var self = this;
             var target = self.getCoords($elem).top;
             var called = false;
 
             self.$htmlbody.stop().animate(
-                {scrollTop: target}, 
-                { 
+                {scrollTop: target},
+                {
                     duration: self.options.speed,
-                    easing: self.options.easing, 
+                    easing: self.options.easing,
                     complete: function() {
                         if (typeof callback === 'function' && !called) {
                             callback();
@@ -1418,77 +1415,77 @@ if (typeof Object.create !== 'function') {
                 }
             );
         },
-        
+
         setTimer: function() {
             var self = this;
-            
+
             self.$window.on('scroll.singlePageNav', function() {
                 self.didScroll = true;
             });
-            
+
             self.timer = setInterval(function() {
                 if (self.didScroll) {
                     self.didScroll = false;
                     self.checkPosition();
                 }
             }, 250);
-        },        
-        
+        },
+
         clearTimer: function() {
             clearInterval(this.timer);
             this.$window.off('scroll.singlePageNav');
             this.didScroll = false;
         },
-        
+
         // Check the scroll position and set the active section
         checkPosition: function() {
             var scrollPos = this.$window.scrollTop();
             var currentSection = this.getCurrentSection(scrollPos);
             this.setActiveLink(currentSection);
-        },        
-        
+        },
+
         getCoords: function($elem) {
             return {
                 top: Math.round($elem.offset().top) - this.options.offset
             };
         },
-        
+
         setActiveLink: function(href) {
             var $activeLink = this.$container.find("a[href='" + href + "']");
-                            
+
             if (!$activeLink.hasClass(this.options.currentClass)) {
                 this.$links.removeClass(this.options.currentClass);
                 $activeLink.addClass(this.options.currentClass);
             }
-        },        
-        
+        },
+
         getCurrentSection: function(scrollPos) {
             var i, hash, coords, section;
-            
+
             for (i = 0; i < this.$links.length; i++) {
                 hash = this.$links[i].hash;
-                
+
                 if ($(hash).length) {
                     coords = this.getCoords($(hash));
-                    
+
                     if (scrollPos >= coords.top - this.options.threshold) {
                         section = hash;
                     }
                 }
             }
-            
+
             // The current section or the first link
             return section || this.$links[0].hash;
         }
     };
-    
+
     $.fn.singlePageNav = function(options) {
         return this.each(function() {
             var singlePageNav = Object.create(SinglePageNav);
             singlePageNav.init(options, this);
         });
     };
-    
+
     $.fn.singlePageNav.defaults = {
         offset: 0,
         threshold: 120,
@@ -1500,5 +1497,5 @@ if (typeof Object.create !== 'function') {
         onComplete: false,
         beforeStart: false
     };
-    
+
 })(jQuery, window, document);
